f31cffb741ee6aa2147cfd62da07b45f6141c1b7
robcanningCU <ac2131@coventry.ac.uk>
2017-05-18 13:45:16 +0100

    twitter bot added

---
+++ twitter_code/twitter_bot.py
@@ -0,0 +1,44 @@
+import tweepy
+import random
+import time
+import string
+
+class TwitterAPI:
+    # this init function sorts out the authentication
+    def __init__(self):
+        consumer_key = "4MDOBG8vdmPI0TwH1QfJ2k"
+        consumer_secret = "7UjfZ5HvmB8pcuEz0R2eDUExxxxxxxxxxxxxxxx"
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        access_token = "732845932816019456-DqdpEZaBOotqQo76Z0DAD5l1mCow"
+        access_token_secret = "k6xq6xxxxxxxxxxxxxxxxxgxfBecYpeS"
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+
+# this is the function that does the tweeting
+    def tweet(self, message):
+        self.api.update_status(status=message)
+
+# below is an array of tweets that later we will randomly choose from
+tweets = [
+    "some #tweet",
+    "other tweet",
+    "some #other tweet",
+    "#foo #tweet",
+    "LOUD tweet",
+    "#tweet #tweet"
+    ]
+# the below if __name__ == "__main__": basically means
+# "if you are running as a normal script rather than an library"
+if __name__ == "__main__":
+    twitter = TwitterAPI()
+    while True: # this means so long as you are running
+                # then just keep looping over the below code
+        tweet = random.choice(tweets)
+        # randomly choose something from the tweets array
+        print(tweet); # print the tweet to screen for testing
+        # keep the below commented while testing -
+        # dont want to spam your twitter account!
+        # twitter.tweet(i)
+        # this calls the tweet function from the TwitterAPI() class
+        # which in this instance it has been named "twitter"
+        time.sleep(10); # wait for 10 seconds
